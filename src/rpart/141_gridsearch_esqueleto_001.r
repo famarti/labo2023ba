1f7a653b7aa54cb4b6b930147bcb72ded675359c
famarti <martinezfacundo94@gmail.com>
2023-08-25 05:10:24 +0000

    final corrida 001

--- src/rpart/141_gridsearch_esqueleto_001.r
+++ src/rpart/141_gridsearch_esqueleto_001.r
@@ -97,9 +97,12 @@ dataset <- dataset[clase_ternaria != ""]
 # genero el archivo para Kaggle
 # creo la carpeta donde va el experimento
 # HT  representa  Hiperparameter Tuning
-dir.create("./exp/", showWarnings = FALSE)
-dir.create("./exp/HT2020/", showWarnings = FALSE)
-archivo_salida <- "./exp/HT2020/gridsearch_001.txt"
+#dir.create("./exp/", showWarnings = FALSE)
+#dir.create("./exp/HT2020/", showWarnings = FALSE)
+#archivo_salida <- "./exp/HT2020/gridsearch.txt"
+dir.create("/home/martinezfacundo94/gridsearch/exp/", showWarnings = FALSE)
+dir.create("/home/martinezfacundo94/gridsearch/exp/HT2020/", showWarnings = FALSE)
+archivo_salida <- "/home/martinezfacundo94/gridsearch/exp//HT2020/gridsearch_001.txt"
 
 # Escribo los titulos al archivo donde van a quedar los resultados
 # atencion que si ya existe el archivo, esta instruccion LO SOBREESCRIBE,
