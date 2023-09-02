3373ba50d7080c52dd146ecc3c12ba88ecf0a6cd
famarti <martinezfacundo94@gmail.com>
2023-09-02 09:49:37 -0300

    feature fraction 0.10

--- src/ArbolesAzarosos/321_arboles_azarosos.r
+++ src/ArbolesAzarosos/321_arboles_azarosos.r
@@ -26,7 +26,7 @@ PARAM$rpart_param <- list(
 
 # parametros  arbol
 # entreno cada arbol con solo 50% de las variables variables
-PARAM$feature_fraction <- 0.5
+PARAM$feature_fraction <- 0.10
 # voy a generar 500 arboles, a mas arboles mas tiempo de proceso y MEJOR MODELO
 #  pero ganancias marginales
 PARAM$num_trees_max <- 500
