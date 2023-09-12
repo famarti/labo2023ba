94ef10ec6ff15ec37c125d9f62ba42d617a00aa5
famarti <martinezfacundo94@gmail.com>
2023-09-11 21:01:48 -0300

    fix chispoteo

--- src/workflow-inicial/601_RUN_correr_workflow_01.r
+++ src/workflow-inicial/601_RUN_correr_workflow_01.r
@@ -12,7 +12,7 @@ options(error = function() {
 # primeros pasos, relativamente rapidos
 source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_01.r")
 source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_01.r")
-source("~/labo2023ba/src/workflow-inicial/631_FE_historia._01r")
+source("~/labo2023ba/src/workflow-inicial/631_FE_historia_01.r")
 source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_01.r")
 
 # ultimos pasos, muy lentos
