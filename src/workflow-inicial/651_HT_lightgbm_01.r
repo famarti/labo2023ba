d7eb0aa748d85c60fc684d4a6a7fc955dc3eac7e
famarti <martinezfacundo94@gmail.com>
2023-09-11 22:02:15 -0300

    termino corregir mocos

--- src/workflow-inicial/651_HT_lightgbm_01.r
+++ src/workflow-inicial/651_HT_lightgbm_01.r
@@ -27,9 +27,9 @@ options(error = function() {
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "HT6510_01"
+PARAM$experimento <- "HT6510_exp01"
 
-PARAM$exp_input <- "TS6410_01"
+PARAM$exp_input <- "TS6410_exp01"
 
 # En caso que se haga cross validation, se usa esta cantidad de folds
 PARAM$lgb_crossvalidation_folds <- 5
